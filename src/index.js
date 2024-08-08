9b3c677858acd0d2012b6b609234d24b0bbda5c0
Egor <egor.marmilev96@gmail.com>
2024-08-08 16:24:13 +0300

    fix: task towel-sort

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 
 // You should implement your task here.
-
 module.exports = function towelSort(matrix) {
   let res = [];
   matrix.forEach((row, index) => {
-    if (index % 2 === 0) {
+    if (index % 2 === 0 || index === 0) {
       res.push(...row);
     } else {
       res.push(...row.reverse());
     }
-  })
-  return res;
+    return res;
+  });
 }
